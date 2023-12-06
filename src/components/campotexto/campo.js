de06fde20958066b30168740a760bdb51463ab6a
Gabriel lima <87135960+Lima404@users.noreply.github.com>
2023-12-06 14:52:26 -0300

    campo texto.js

--- src/components/campotexto/campo.js
+++ src/components/campotexto/campo.js
@@ -1,10 +1,17 @@
 import './campo.css'
 
 const CampoTexto = (props) => {
+
+    const aoDigitar = (evento) => {
+        props.aoAlterado(evento.target.value)
+    }
+
     return(
         <div className='card'>
-            <label>{props.label}</label>
-            <input placeholder={props.placeholder}/>
+            <label>
+                {props.label}
+            </label>
+            <input value={props.valor} onChange={aoDigitar} required={props.prior} placeholder={props.placeholder}/>
         </div>
     )
 }
