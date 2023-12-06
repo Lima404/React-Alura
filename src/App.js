00fb8c85b2abfd3dd079900afdff17f2d742f739
Gabriel lima <87135960+Lima404@users.noreply.github.com>
2023-12-06 14:51:34 -0300

    add app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
+import { useState } from 'react';
 import Banner from './components/Banner/Banner';
 import Formulario from './components/form/form';
 
 function App() {
+
+  const [colaboradores, setColaboradores] = useState([])
+
+  const aoNovoColaboradorAdcionado = (colaborador) => {
+    console.log(colaborador)
+  }
+
   return (
     <div className="App">
       <Banner />
-      <Formulario/>
+      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdcionado(colaborador)}/>
     </div>
   );
 }
