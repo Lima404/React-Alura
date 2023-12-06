d974beb86d50db360d45df08f8b8c0d8dabf7d48
Gabriel lima <87135960+Lima404@users.noreply.github.com>
2023-12-06 14:52:54 -0300

    form.js

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,6 +1,8 @@
 import './form.css'
 import CampoTexto from '../campotexto/campo'
 import ListaSuspensa from '../ListaSuspensa'
+import Botao from '../botao'
+import { useState } from 'react'
 
 const Formulario = () => {
 
@@ -12,19 +14,50 @@ const Formulario = () => {
         'Mobile',
         'Inovação e Gestão'
     ]
+
+    const [nome, setNome] = useState('')
+    const [cargo, setCargo] = useState('')
+    const [imagem, setImagem] = useState('')
+    const [time, setTime] = useState('')
+    
+    
+    const aoSalvar = (evento) => {
+        evento.preventDefault();
+        console.log('Form foi submetido =>', nome, cargo, imagem, time)
+    }
+    
     return(
         <section>
-                <form>
+                <form onSubmit = {aoSalvar}>
                     <h2>Preencha os cargos para criar o card do colaborador!</h2>
                     <div className='box-interno'>
                         <div>
-                            <CampoTexto label="Nome" placeholder="Digite seu nome"/>
-                            <CampoTexto label="Cargo" placeholder="Digite seu cargo"/>
-                            <CampoTexto label="Imagem" placeholder="Digite seuo endereço de imagem"/>
-                            <ListaSuspensa itens={times}/>
-                        </div>
-                        <div className='buttom'>
-                            <button>Criar card</button>
+                            <CampoTexto 
+                                prior={true} 
+                                label="Nome" 
+                                placeholder="Digite seu nome..."
+                                valor={nome}
+                                aoAlterado={valor => setNome(valor)}/>
+                            <CampoTexto 
+                                prior={true} 
+                                label="Cargo" 
+                                placeholder="Digite seu cargo..."
+                                valor={cargo}
+                                aoAlterado={valor => setCargo(valor)}/>
+                            <CampoTexto 
+                                label="Imagem" placeholder="Digite seuo endereço de imagem..."
+                                valor={imagem}
+                                aoAlterado={valor => setImagem(valor)}/>
+                            <ListaSuspensa 
+                                prior={true}
+                                label="Time" 
+                                itens={times}
+                                valor={time}
+                                aoAlterado={valor => setTime(valor)}
+                                />
+                            <Botao>
+                                Criar Card
+                            </Botao>
                         </div>
                     </div>
                 </form>
