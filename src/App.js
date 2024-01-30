e068eb7e3846c6d0968f66eb362870372766e719
Lima404 <gabriel.lima2307@uol.com.br>
2024-01-30 17:13:18 -0300

    update app.js

--- src/App.js
+++ src/App.js
@@ -9,37 +9,37 @@ function App() {
     {
       nome: 'Programação',
       corPrimaria: '#57C278',
-      CorSecundaria: '#D9F7E9'
+      corSecundaria: '#D9F7E9'
     },
     {
       nome: 'Front-end',
       corPrimaria: '#82CFFA',
-      CorSecundaria: '#E8F8FF'
+      corSecundaria: '#E8F8FF'
     },
     {
       nome: 'Data Science',
       corPrimaria: '#A6D157',
-      CorSecundaria: '#F0F8E2'
+      corSecundaria: '#F0F8E2'
     },
     {
       nome: 'Devops',
       corPrimaria: '#E06B69',
-      CorSecundaria: '#FDE7E8'
+      corSecundaria: '#FDE7E8'
     },
     {
       nome: 'UX e Desing',
       corPrimaria: '#DB6EBF',
-      CorSecundaria: '#FAE9F5'
+      corSecundaria: '#FAE9F5'
     },
     {
       nome: 'Mobile',
       corPrimaria: '#FFBA05',
-      CorSecundaria: '#FFF5D9'
+      corSecundaria: '#FFF5D9'
     },
     {
       nome: 'Inovação e Gestão',
       corPrimaria: '#FF8A29',
-      CorSecundaria: '#FFEEDF'
+      corSecundaria: '#FFEEDF'
     },
   ]
 
@@ -47,19 +47,22 @@ function App() {
 
   const aoNovoColaboradorAdcionado = (colaborador) => {
     console.log(colaborador)
+    setColaboradores([...colaboradores, colaborador])
   }
 
   return (
     <div className="App">
       <Banner />
-      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdcionado(colaborador)}/>
-      <Time nome="Programação"/>
-      <Time nome="Front-end"/>
-      <Time nome="Data Science"/>
-      <Time nome="Devops"/>
-      <Time nome="Ux & Design"/>
-      <Time nome="Mobile"/>
-      <Time nome="Inovaçao e Gestão"/>
+      <Formulario times={times.map(time => time.nome)} aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdcionado(colaborador)}/>
+      
+      {times.map (time => <Time 
+        key={time.nome}
+        nome={time.nome}
+        corPrimaria={time.corPrimaria} 
+        corSecundaria={time.corSecundaria}
+        colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
+        />)}
+
     </div>
   );
 }
